67a517a463e2bd6c3321279c3ad53cff45d15834
Leeangelo Riley <59509041+LeCoding-tech@users.noreply.github.com>
2021-05-28 23:48:52 -0400

    Update footer.js

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -6,16 +6,7 @@ const Footer = () => {
     <>
       <div className="skills-body">
         <head>
-          <link
-            rel="stylesheet"
-            href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
-            integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP"
-            crossorigin="anonymous"
-          />
-          <link
-            rel="stylesheet"
-            href="https://use.fontwesome.com/releases/v5.4.1/css/all.css"
-          />
+          <script src="https://use.fontawesome.com/740dccf130.js"></script>
         </head>
         <footer id="footer">
           <div className="footer--text">
